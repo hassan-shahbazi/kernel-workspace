ed011ffe7f184cbd5979b80697969955ec3e3637
Hassan Shahbazi <h-shahbazi@hotmail.com>
2021-01-30 09:56:25 +0200

    task01: basic kernel module

---
+++ task_01/ktask01.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_AUTHOR("Hassan Shahbazi");
+MODULE_DESCRIPTION("Linux Kernel Mentorship task 01");
+
+static int __init task_init(void) {
+    printk(KERN_INFO "Hello World!\n");
+    return 0;
+}
+
+static void __exit task_exit(void) {
+    printk(KERN_INFO "Goodby!\n");
+}
+
+module_init(task_init);
+module_exit(task_exit);
